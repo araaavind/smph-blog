araaavind/smph-blog

commit 5e458417cab6641987d5c9a5ee38503ee0025a8b

offline view bugfix and online image reload

lib/features/blog/data/models/blog_model.dart
@@ -21,6 +21,7 @@ class BlogModel extends Blog {
       'image_url': imageUrl,
       'topics': topics,
       'updated_at': updatedAt.toIso8601String(),
+      'user_name': userName ?? '',
     };
   }
 
@@ -35,6 +36,7 @@ class BlogModel extends Blog {
       updatedAt: map['updated_at'] == null
           ? DateTime.now()
           : DateTime.parse(map['updated_at']),
+      userName: map['user_name'] == null ? '' : map['user_name'] as String,
     );
   }
 

lib/features/blog/presentation/bloc/blog_bloc.dart
@@ -44,7 +44,9 @@ class BlogBloc extends Bloc<BlogEvent, BlogState> {
   }
 
   void _onGetAllBlogs(
-      BlogGetAllBlogsEvent event, Emitter<BlogState> emit) async {
+    BlogGetAllBlogsEvent event,
+    Emitter<BlogState> emit,
+  ) async {
     final res = await _getAllBlogs(NoParams());
 
     res.fold(

lib/features/blog/presentation/pages/blog_viewer_page.dart
@@ -24,7 +24,7 @@ class BlogViewerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: BlocListener<NetworkCubit, NetworkState>(
+      body: BlocConsumer<NetworkCubit, NetworkState>(
         listener: (context, state) {
           if (state is NetworkDisconnected) {
             showSnackbar(context, Constants.networkDisconnectedMessage);
@@ -32,58 +32,79 @@ class BlogViewerPage extends StatelessWidget {
             showSnackbar(context, Constants.networkConnectedMessage);
           }
         },
-        child: Scrollbar(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    blog.title,
-                    style: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w900,
-                      color: AppPalette.whiteColor,
+        builder: (context, state) {
+          final blogImageWidget = state is NetworkConnected
+              ? Image.network(blog.imageUrl)
+              : Container(
+                  height: 80,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: AppPalette.blogCardColor,
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Images are not loaded when you are offline',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w100,
+                      ),
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  Text(
-                    'By ${blog.userName}',
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.white,
+                );
+
+          return Scrollbar(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      blog.title,
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w900,
+                        color: AppPalette.whiteColor,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 5),
-                  Text(
-                    '${formatDate(blog.updatedAt, 'd MMM, yyyy')} . ${calculateReadingTime(blog.content)} min',
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.white70,
+                    const SizedBox(height: 20),
+                    Text(
+                      'By ${blog.userName}',
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 20),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(8.0),
-                    child: Image.network(blog.imageUrl),
-                  ),
-                  const SizedBox(height: 20),
-                  Text(
-                    blog.content,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      color: Colors.white,
-                      height: 1.7,
+                    const SizedBox(height: 5),
+                    Text(
+                      '${formatDate(blog.updatedAt, 'd MMM, yyyy')} . ${calculateReadingTime(blog.content)} min',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white70,
+                      ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 20),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: blogImageWidget,
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      blog.content,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        height: 1.7,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }

lib/features/blog/presentation/widgets/blog_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:semaphore/core/common/cubits/cubit/network_cubit.dart';
 import 'package:semaphore/core/theme/app_palette.dart';
 import 'package:semaphore/core/utils/calculate_reading_time.dart';
 import 'package:semaphore/features/blog/domain/entities/blog.dart';
@@ -20,84 +22,175 @@ class BlogCard extends StatelessWidget {
       onTap: () {
         Navigator.push(context, BlogViewerPage.route(blog));
       },
-      child: Stack(
-        children: [
-          Container(
-            height: 200,
-            margin: const EdgeInsets.all(16),
-            padding: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                  blog.imageUrl,
-                ),
+      child: BlocBuilder<NetworkCubit, NetworkState>(
+        builder: (context, state) {
+          if (state is NetworkConnected) {
+            return OnlineCard(blog: blog, color: color);
+          }
+          return OfflineCard(blog: blog, color: color);
+        },
+      ),
+    );
+  }
+}
+
+class OnlineCard extends StatelessWidget {
+  const OnlineCard({
+    super.key,
+    required this.blog,
+    required this.color,
+  });
+
+  final Blog blog;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: 200,
+          margin: const EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(
+                blog.imageUrl,
               ),
-              borderRadius: BorderRadius.circular(8),
+            ),
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+        Container(
+          height: 200,
+          width: double.infinity,
+          margin: const EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(8),
+            gradient: LinearGradient(
+              colors: [
+                Colors.black.withOpacity(0.9),
+                Colors.black.withOpacity(0.5),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              stops: const [0, 0.75],
             ),
           ),
-          Container(
-            height: 200,
-            width: double.infinity,
-            margin: const EdgeInsets.all(16),
-            padding: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              color: color,
-              borderRadius: BorderRadius.circular(8),
-              gradient: LinearGradient(
-                colors: [
-                  Colors.black.withOpacity(0.9),
-                  Colors.black.withOpacity(0.5),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                stops: const [0, 0.75],
-              ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        children: blog.topics
-                            .map(
-                              (e) => Padding(
-                                padding: const EdgeInsets.only(right: 10.0),
-                                child: Chip(
-                                  label: Text(
-                                    e,
-                                    style: const TextStyle(
-                                      color: AppPalette.whiteColor,
-                                    ),
-                                  ),
-                                  color: const WidgetStatePropertyAll(
-                                    AppPalette.gradient2,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: blog.topics
+                          .map(
+                            (e) => Padding(
+                              padding: const EdgeInsets.only(right: 10.0),
+                              child: Chip(
+                                label: Text(
+                                  e,
+                                  style: const TextStyle(
+                                    color: AppPalette.whiteColor,
                                   ),
                                 ),
+                                color: const WidgetStatePropertyAll(
+                                  AppPalette.gradient2,
+                                ),
                               ),
-                            )
-                            .toList(),
-                      ),
+                            ),
+                          )
+                          .toList(),
                     ),
-                    Text(
-                      blog.title,
-                      style: const TextStyle(
-                        color: AppPalette.whiteColor,
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  ),
+                  Text(
+                    blog.title,
+                    style: const TextStyle(
+                      color: AppPalette.whiteColor,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
                     ),
-                  ],
-                ),
-                Text('${calculateReadingTime(blog.content)} min'),
-              ],
-            ),
+                  ),
+                ],
+              ),
+              Text('${calculateReadingTime(blog.content)} min'),
+            ],
           ),
+        ),
+      ],
+    );
+  }
+}
+
+class OfflineCard extends StatelessWidget {
+  const OfflineCard({
+    super.key,
+    required this.blog,
+    required this.color,
+  });
+
+  final Blog blog;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: double.infinity,
+      margin: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: blog.topics
+                      .map(
+                        (e) => Padding(
+                          padding: const EdgeInsets.only(right: 10.0),
+                          child: Chip(
+                            label: Text(
+                              e,
+                              style: const TextStyle(
+                                color: AppPalette.whiteColor,
+                              ),
+                            ),
+                            color: const WidgetStatePropertyAll(
+                              AppPalette.gradient2,
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+              Text(
+                blog.title,
+                style: const TextStyle(
+                  color: AppPalette.whiteColor,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Text('${calculateReadingTime(blog.content)} min'),
         ],
       ),
     );

commit 627deb89cc3c7a8115c510eb2cb2703c3f949273

fixed bug in blog upload caused by 5e458417

lib/features/blog/data/datasources/blog_local_datasource.dart
@@ -16,7 +16,12 @@ class BlogLocalDatasourceImpl implements BlogLocalDatasource {
     List<BlogModel> blogs = [];
     box.read(() {
       for (int i = 0; i < box.length; i++) {
-        blogs.add(BlogModel.fromJson(box.get(i.toString())));
+        final blogObject = box.get(i.toString());
+        blogs.add(
+          BlogModel.fromJson(blogObject).copyWith(
+            userName: blogObject['user_name'],
+          ),
+        );
       }
     });
     return blogs;
@@ -27,7 +32,9 @@ class BlogLocalDatasourceImpl implements BlogLocalDatasource {
     box.clear();
     box.write(() {
       for (int i = 0; i < blogs.length; i++) {
-        box.put(i.toString(), blogs[i].toJson());
+        final blogObject = blogs[i].toJson();
+        blogObject['user_name'] = blogs[i].userName;
+        box.put(i.toString(), blogObject);
       }
     });
   }

lib/features/blog/data/models/blog_model.dart
@@ -21,7 +21,6 @@ class BlogModel extends Blog {
       'image_url': imageUrl,
       'topics': topics,
       'updated_at': updatedAt.toIso8601String(),
-      'user_name': userName ?? '',
     };
   }
 
@@ -36,7 +35,6 @@ class BlogModel extends Blog {
       updatedAt: map['updated_at'] == null
           ? DateTime.now()
           : DateTime.parse(map['updated_at']),
-      userName: map['user_name'] == null ? '' : map['user_name'] as String,
     );
   }
 

lib/features/profile/presentation/bloc/profile_bloc.dart
@@ -19,7 +19,6 @@ class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
         _appUserCubit = appUserCubit,
         super(ProfileInitial()) {
     on<ProfileEvent>((event, emit) async {
-      print(event);
       final res = await _getCurrentUserProfileData(NoParams());
 
       res.fold(